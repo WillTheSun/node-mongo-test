f28381e09c16d247ff28685f4b6ea27b9c9aaab9
William Sun <wsun@acorns.com>
2018-06-06 11:23:31 -0700

    add requests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi')
 const mongoose = require('mongoose')
+const Painting = require('./models/painting')
 
 const init = async() => {
   mongoose.connect('mongodb://admin:password1@ds251240.mlab.com:51240/mongoose_test')
@@ -13,13 +14,33 @@ const init = async() => {
     host: 'localhost'
   })
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(req, res) {
-      return '<h1> test </h1>'
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: function(req, res) {
+        return '<h1> test </h1>'
+      }
+    },
+    {
+      method: 'GET',
+      path: '/api/v1/paintings',
+      handler: (req, res) => Painting.find()
+    },
+    {
+      method: 'POST',
+      path: '/api/v1/paintings',
+      handler: (req, res) => {
+        const { name, url, techniques } = req.payload
+        const painting = new Painting({
+          name,
+          url,
+          techniques
+        })
+        return painting.save()
+      }
     }
-  })
+  ])
 
   await server.start()
 
